7804bd5af6963b5395fed893b2294553872a594f
JD Dudley <jason@jddudley.com>
2018-03-17 12:55:56 -0600

    Home Page Improvements, Meta Descriptions in Routes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,10 +4,16 @@
     <img src="../assets/logo_dk.png" class="logo hidden-xs-only" alt="Five Star Service Pros">
     <div>
       <h1>Plumbing, Sewer and Drain Cleaning Experts</h1>
-      <p>Welcome to Five Star Service Pros, the most reliable and affordable choice for your plumbing, sewer, septic, and drain cleaning needs in Boise and surrounding areas. With service available 24 hours a day, you know you can count on emergency services from a locally owned company.</p>
-      <p>All services are performed by experienced plumbers with years of experience in every area of the plumbing and pipe service trade. Whether it's a kitchen sink replacement or a complete drain cleaning service, you can feel confident that our plumbing services are performed in the quickest, safest, and most effective manner.</p>
+      <p>Welcome to Five Star Service Pros, the most reliable and affordable choice for your plumbing, sewer, septic, and drain
+        cleaning needs in Boise and surrounding areas. With service available 24 hours a day, you know you can count on emergency
+        services from a locally owned company.</p>
+      <p>All services are performed by experienced plumbers with years of experience in every area of the plumbing and pipe
+        service trade. Whether it's a kitchen sink replacement or a complete drain cleaning service, you can feel confident
+        that our plumbing services are performed in the quickest, safest, and most effective manner.</p>
       <h2>Who is Five Star Service Pros?</h2>
-      <p>Five Star Service Pros is a local female-owned company with extensive experience in all areas of residential and commercial plumbing. Quality training, experience, and equipment are critical to ensuring that our customers receive the highest-level service on every job.</p>
+      <p>Five Star Service Pros is a local female-owned company with extensive experience in all areas of residential and commercial
+        plumbing. Quality training, experience, and equipment are critical to ensuring that our customers receive the highest-level
+        service on every job.</p>
     </div>
     <!-- Main Services -->
     <h1>Services Include</h1>
@@ -30,6 +36,23 @@
         </v-layout>
       </v-container>
     </div>
+    <!-- home advisor badges -->
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs4 text-xs-center>
+          <a href="http://www.homeadvisor.com/c.Plumbing.Boise.ID.-12058.html#spid=61604027"><img alt="Top Rated Contractor - 5 Star Service Pros, LLC" src="http://www.homeadvisor.com/images/sp-badges/toprated-solid-border.png?sp=61604027&key=29a3cd3c7e116acee983f0a2a3439c88"
+            /></a>
+        </v-flex>
+        <v-flex xs4 text-xs-center>
+          <a href="http://www.homeadvisor.com/c.Plumbing.Boise.ID.-12058.html#spid=61604027"><img alt="5 Star Service Pros, LLC - HomeAdvisor Elite Service" src="http://www.homeadvisor.com/images/sp-badges/elite-solid-border.png?sp=61604027&key=29a3cd3c7e116acee983f0a2a3439c88"
+            /></a>
+        </v-flex>
+        <v-flex xs4 text-xs-center>
+          <a href="http://www.homeadvisor.com/rated.5StarServiceProsLLC.61604027.html"><img alt="Rated & Reviewed HomeAdvisor Pro" src="http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=61604027&key=29a3cd3c7e116acee983f0a2a3439c88"
+            /></a>
+        </v-flex>
+      </v-layout>
+    </v-container>
     <!-- service truck image -->
     <img src="../assets/service_truck.jpg" alt="Service Trucks Operating Throughout the Treasure Valley" class="logo">
   </div>
@@ -97,7 +120,8 @@ export default {
 </script>
 
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,12 @@ export default new Router({
       component: Home,
       meta: {
         title: titleStart + ' - Boise Plumbing, Sewer, and Drain',
-        description: 'Five Star Service Pros is Boise\'s  most reliable and affordable choice for your plumbing, sewer, and drain cleaning needs. With service available 24 hours a day, you know you can count on emergency services from a locally owned company.'
+        metaTags: [
+          {
+            name: 'description',
+            content: 'Five Star Service Pros is Boise\'s  most reliable and affordable choice for your plumbing, sewer, and drain cleaning needs. With service available 24 hours a day, you know you can count on emergency services from a locally owned company.'
+          }
+        ]
       }
     },
     {
@@ -35,7 +40,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Contact Us'
       },
-      description: 'Contact Five Star Service Pros for honest and reliable answers to all your plumbing and sewer needs. We charge by the job, not the hour, so your bill will never surprise you.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Contact Five Star Service Pros for honest and reliable answers to all your plumbing and sewer needs. We charge by the job, not the hour, so your bill will never surprise you.'
+        }
+      ]
     },
     {
       path: '/appointments',
@@ -44,7 +54,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Schedule an Appointment'
       },
-      description: 'Book an appointment to get Boise\'s best plumbing experts at your door to solve any pluming, sewer, or drain troubles you\'re having.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Book an appointment to get Boise\'s best plumbing experts at your door to solve any pluming, sewer, or drain troubles you\'re having.'
+        }
+      ]
     },
     {
       path: '/thank-you',
@@ -53,7 +68,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Thank You!'
       },
-      description: 'Thanks for contacting Five Star Service Pros! You\'re about to hear back from the most reliable plumbers in Boise.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Thanks for contacting Five Star Service Pros! You\'re about to hear back from the most reliable plumbers in Boise.'
+        }
+      ]
     },
     {
       path: '/services',
@@ -62,7 +82,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Services'
       },
-      description: 'Five Star Service Pros has experience and expertise in all areas of plumbing, sewer, and drain cleaning services. Let us give you the best treatment in Boise.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Five Star Service Pros has experience and expertise in all areas of plumbing, sewer, and drain cleaning services. Let us give you the best treatment in Boise.'
+        }
+      ]
     },
     {
       path: '/services/drain-cleaning',
@@ -71,7 +96,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Drain Cleaning'
       },
-      description: 'Whether it\s a clogged sink or tub, or an entire drainage system backed up, Five Star Service Pros has the experience and equipment to repair any damage and get you back to normal.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Whether it\s a clogged sink or tub, or an entire drainage system backed up, Five Star Service Pros has the experience and equipment to repair any damage and get you back to normal.'
+        }
+      ]
     },
     {
       path: '/services/residential-plumbing',
@@ -80,7 +110,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Residential Plumbing'
       },
-      description: 'Five Star Plumbing has the most reliable service in the Treasure Valley for all your residential plumbing needs. Kitchen, bath, laundry, and everything in between. Let us provide the service you deserve.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Five Star Plumbing has the most reliable service in the Treasure Valley for all your residential plumbing needs. Kitchen, bath, laundry, and everything in between. Let us provide the service you deserve.'
+        }
+      ]
     },
     {
       path: '/services/commercial-plumbing',
@@ -89,7 +124,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Commercial Plumbing'
       },
-      description: 'Five Star serves numerous local businesses as the most reliable on-call plumber in the valley. Let us provide the assurance that you\'ll always have immediate fixes to any commercial plumbing problems that come up.'
+      metaTags: [
+        {
+          name: 'description',
+          content: 'Five Star serves numerous local businesses as the most reliable on-call plumber in the valley. Let us provide the assurance that you\'ll always have immediate fixes to any commercial plumbing problems that come up.'
+        }
+      ]
     },
     {
       path: '/services/flood-repair',
@@ -98,7 +138,12 @@ export default new Router({
       meta: {
         title: titleStart + ' - Flood Repair & Restoration'
       },
-      description: '24/7 Emergency flood repair and restoration. Five Star Service Pros is always here to help minimize damage and ensure you get back to normal as quickly as possible.'
+      metaTags: [
+        {
+          name: 'description',
+          content: '24/7 Emergency flood repair and restoration. Five Star Service Pros is always here to help minimize damage and ensure you get back to normal as quickly as possible.'
+        }
+      ]
     },
     {
       path: '/404',
