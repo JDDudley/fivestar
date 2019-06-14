04cd25baf018ef85cecf89aa36aa4fb85b1046bc
Jason Dudley <jason@jddudley.com>
2019-06-13 21:17:02 -0600

    Meeting Updates

--- README.md
+++ README.md
@@ -27,3 +27,24 @@ Commercial Plumbing
 Mobile Home Plumbing
 
 ### Areas
+
+### FiveStar Meeting Notes
+#### April 19, 2018
+
+##### Add Appliance Pages
+- Use images from Pages from manufacturers that are typically used
+- Water Heater Page
+	- Rheem, Bradford & White
+	- Copy from Option One
+= Tankless Water Heater Page
+	- Navien
+- Gas Piping & Converting Appliances to Gas
+	- List all common Idaho appliances
+	- Revelant, current content
+	- Option One sourcing
+
+##### General Changes
+- Home Page/Template
+	- 24 hour/weekend/emergency/holiday/afterhour no extra charge
+- Stress 24 hour, weekends, holidays, and afterhours
+	- Page stressing no extra charge
---
+++ src/components/Services/WaterHeaters.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="component">
+    <h1>{{ service }}</h1>
+    <p>{{ description }}</p>
+    <v-container grid-list-md>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card color="primary">
+            <v-card-title>
+              <h2 class="primary-card-title">SERVICES</h2>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm6 v-for="specificService in specificServices" :key="specificService.title">
+          <v-card>
+            <v-card-title primary-title>
+              <h3 class="headline">{{ specificService.title }}</h3>
+            </v-card-title>
+            <v-card-text>
+              <ul class="no-bullet-list">
+                <li v-for="item in specificService.list"><v-divider></v-divider>{{ item }}</li>
+              </ul>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WaterHeaters",
+  data() {
+    return {
+      service: "Water Heaters",
+      description:
+        "Five Star Service Pros has experience with every type of water heating system.",
+      specificServices: [
+        {
+          title: "Water Damage Restoration",
+          list: [
+            "Inspection & Damage Assessment",
+            "Water Removal/Water Extraction",
+            "Drying and Dehumidification",
+            "Cleaning and Repair",
+            "Restoration"
+          ]
+        }
+      ]
+    };
+  },
+  metaInfo: {
+    title: 'Five Star Service Pros - Flood Repair',
+    meta: [
+      {name: 'description', content: '24/7 Emergency flood repair and restoration. Five Star Service Pros is always here to help minimize damage and ensure you get back to normal as quickly as possible.'}
+    ]
+  }
+};
+</script>
+
+<style>
+
+</style>
